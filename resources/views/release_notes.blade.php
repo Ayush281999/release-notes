363b9273fd7fdcfe7315b97bd24a0e7fca813025
Ayush Kumar <ayushk.itpath@gmail.com>
2024-12-30 20:25:34 +0530

    update route and manage slots

--- resources/views/release_notes.blade.php
+++ resources/views/release_notes.blade.php
@@ -1,6 +1,4 @@
-@extends('layouts.app')
 
-@section('content')
     <div class="container">
         <h1>Release Notes</h1>
         <table class="table">
@@ -22,4 +20,3 @@
             </tbody>
         </table>
     </div>
-@endsection
