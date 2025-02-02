b12ff4ea9356654d1b09c6508e7eb36c3e6b92d7
Ayush Kumar <ayushk.itpath@gmail.com>
2025-02-02 18:44:51 +0530

    Update relesenoted

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/release-notes', [ReleaseNoteController::class, 'index']);
+Route::get('/all-release-notes', [ReleaseNoteController::class, 'index']);
 
 
 require __DIR__.'/auth.php';
